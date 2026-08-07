0a4c288cb1ceebd63cca704acffa0fa8935cb3b5
niyonsabaemery3-coder <niyonsabaemery3@gmail.com>
2026-08-07 13:51:02 -0700

    fix vite allowed host for render

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   preview: {
-    allowedHosts: ['saith-babeth-tss.onrender.com'],
+    allowedHosts: true,
   },
 
   base: process.env.BASE_PATH || '/',
